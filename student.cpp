f5bf7f48398c52034d4e902a1d2437d3755f3bc2
ZHIYUNING <38142057+zhiyuning@users.noreply.github.com>
2019-01-27 17:41:47 -0800

    Add files via upload

--- student.cpp
+++ student.cpp
@@ -1,56 +1,66 @@
 #include "student.h"
 #include <string>
 #include <cstring>
+#include <sstream>
 
 Student::Student(const char * const name, int perm) {
-  this->setName("another stub");
+  this->name=NULL;
+  this->setName(name);
+  this->setPerm(perm);
 }
 
 int Student::getPerm() const {
-  return -42;
+  return this->perm;
 }
 
 const char * const Student::getName() const {
-  return "stub";
+  return name;
 }
 
 void Student::setPerm(const int permNumber) {
+  this->perm=permNumber;
 }
 
 void Student::setName(const char * const name) {
-  this->name = new char[strlen("stub")+1];
-  strcpy(this->name,"stub");
+  if(this->name!=NULL){
+    delete[] this->name;
+  }
+  this->name = new char[strlen(name)+1];
+  strcpy(this->name,name);
 }
 
 
 Student::Student(const Student &orig) {
-  this->setName("yet another stub");
-  this->setPerm(-42);
+  this->name=NULL;
+  this->setName(orig.name);
+  this->setPerm(orig.perm);
 }
 
 Student::~Student() {
-
+  delete[] this->name;
 }
 
 Student & Student::operator=(const Student &right) {
   // The next two lines are standard, and you should keep them.
-  // They avoid problems with self-assignment where you might free up 
+  // They avoid problems with self-assignment where you might free up
   // memory before you copy from it.  (e.g. x = x)
 
-  if (&right == this) 
+  if (&right == this)
     return (*this);
 
-  // TODO... Here is where there is code missing that you need to 
-  // fill in...
-
+  this->setName(right.name);
+  this->setPerm(right.perm);
 
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
-  return (*this); 
+  return (*this);
 
 }
 
 std::string Student::toString() const {
-  return "tostring stub";
+  std::ostringstream oss;
+  oss <<"["
+      <<this->getName()<<","
+      <<this->getPerm()<<"]";
+  return oss.str();
 }
-
--- studentRoll.cpp
+++ studentRoll.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 #include "studentRoll.h"
 
 StudentRoll::StudentRoll() {
@@ -6,41 +7,125 @@ StudentRoll::StudentRoll() {
 }
 
 void StudentRoll::insertAtTail(const Student &s) {
-  // STUB
+  Node *node=new Node;
+  Student *ne=new Student(s);
+  node->s=ne;
+  node->next=NULL;
+  if(this->head==NULL){
+    head=node;
+    tail=node;
+  }else{
+    tail->next=node;
+    tail=node;
+  }
 }
 
 std::string StudentRoll::toString() const {
-  return "stub";
+  std::ostringstream oss;
+
+  if (head==NULL) {
+    oss <<"["
+        <<"]";
+  }else{
+    Node *temp=head;
+    oss <<"["
+        <<head->s->toString();
+    while(temp->next!=NULL){
+      temp=temp->next;
+      oss <<","<<temp->s->toString();
+    }
+    oss <<"]";
+  }
+  return oss.str();
 }
 
 StudentRoll::StudentRoll(const StudentRoll &orig) {
-  // STUB
-  head = tail = NULL;
+  if(orig.head != 0){
+    Node *prev=new Node;
+    Student *ne=new Student(*orig.head->s);
+    prev->s=ne;
+    if(orig.head!=orig.tail){
+      head=prev;
+      for(Node *i=orig.head->next;i->next!=0;i=i->next) {
+        Node *temp=new Node;
+        Student *ne=new Student(*orig.head->s);
+        temp->s=ne;
+
+        prev->next= temp;
+        prev=prev->next;
+      }
+      Node *temp2=new Node;
+      Student *ne1=new Student(*orig.tail->s);
+      temp2->s=ne1;
+
+      prev->next=temp2;
+      tail=temp2;
+    }else{
+      head = prev;
+      tail = prev;
+    }
+  }else{
+    head = 0;
+    tail = 0;
+  }
+//  Node *curr=orig.head;
+//  while(curr->next!=NULL){
+//    curr=curr->next;
+//    insertAtTail(* curr->s);
+//  }
+//  insertAtTail(* curr->s);
 }
 
+
+
+
+
 StudentRoll::~StudentRoll() {
-  // STUB
+  if(head!=NULL){
+    Node *temp=head->next;
+    delete head;
+    head=temp;
+  }
 }
 
 StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
   // The next two lines are standard, and you should keep them.
-  // They avoid problems with self-assignment where you might free up 
+  // They avoid problems with self-assignment where you might free up
   // memory before you copy from it.  (e.g. x = x)
 
-  if (&right == this) 
+  if (&right == this){
     return (*this);
+  }
+  if(right.head!=NULL){
+    Node *prev=new Node;
+    Student *ne=new Student(*right.head->s);
+    prev->s=ne;
+    if(right.head!=right.tail){
+      head=prev;
+      for(Node *i=right.head->next;i->next!=0;i=i->next) {
+        Node *temp=new Node;
+        Student *ne=new Student(*right.head->s);
+        temp->s=ne;
 
-  // TODO... Here is where there is code missing that you need to 
-  // fill in...
+        prev->next=temp;
+        prev=prev->next;
+      }
+      Node *temp2=new Node;
+      Student *ne1=new Student(*right.tail->s);
+      temp2->s=ne1;
 
-
+      prev->next=temp2;
+      tail=temp2;
+    }else{
+      head = prev;
+      tail = prev;
+    }
+  }else{
+    head = 0;
+    tail = 0;
+  }
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
-  return (*this); 
-  
+  return (*this);
+
 }
-
-
-
-
-
